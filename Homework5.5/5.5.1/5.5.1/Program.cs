3377541fbec989d1c35eefddec493aca0392ce83
Myryty <134264732+Myryty@users.noreply.github.com>
2024-04-04 15:12:11 +0300

    Add files via upload

---
+++ Homework5.5/5.5.1/5.5.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._1
+{
+    class Program
+    {
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+        static void EnterTextPhrase()
+        {
+            Console.WriteLine("Введите любое предложение (слова разделены пробелом): ");
+        }
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        static void PrintWords(string[] words)
+        {
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+        }
+        static void Main(string[] args)
+        {
+            EnterTextPhrase();
+            string text = Convert.ToString(Console.ReadLine());
+            string[] words = SplitText(text);
+            PrintWords(words);
+            Pause();
+        }
+    }
+}
---
+++ Homework5.5/5.5.2/5.5.2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5._2
+{
+    class Program
+    {
+        static void Pause()
+        {
+            Console.ReadKey();
+        }
+        static string ReverseWords(string inputPhrase)
+        {
+            string[] words = SplitText(inputPhrase);
+            Array.Reverse(words);
+            string reversedPhrase = JoinText(words);
+            return reversedPhrase;
+        }
+        static string[] SplitText(string text)
+        {
+            return text.Split(' ');
+        }
+        static string JoinText(string[] words)
+        {
+            return string.Join(" ", words);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение:");
+            string inputPhrase = Console.ReadLine();
+            string reversedPhrase = ReverseWords(inputPhrase);
+            Console.WriteLine("Предложение задом наперед:");
+            Console.WriteLine(reversedPhrase);
+            Pause();
+        }
+
+
+    }
+}
